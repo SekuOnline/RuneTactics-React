c513753690221d67106fbad2f3a73d8719fa89ba
David <davidsedarous@cmail.cartleon.ca>
2024-10-04 18:40:31 -0400

    Additional Basic Styling + Removing Bloat Tests, Added default deckcodes (temporary)

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import rtSets from "./set_json/setBarrel.js";
 
 export default App;
 
-const regionColors = {"Bandle City":"rgba(159, 192, 0, 1)", "Targon":"rgba(105, 57, 204, 1)", "Shurima":"rgba(238, 192, 30,1)", "Noxus": "rgba(182, 0, 30,1)", "Freljord":"rgba(135, 211, 233, 1)", "Demacia":"rgba(233, 218, 179, 1)", "Ionia":"rgba(248, 107, 179, 1)",  "Bilgewater":"rgba(181, 69, 44,1)", "Piltover & Zaun":"rgba(255, 129, 53,1)", "Runeterra":"rgba(129, 112, 74, 1)", "Shadow Isles":"rgba(0, 163, 132,1)"}
-
-
 function App() {
   // const [darkMode, setDarkMode] = useState(false);
   // const bgColor = darkMode ? "black" : "white";
@@ -20,7 +17,12 @@ function App() {
 
 function DeckContainer() {
   const [textInput, setTextInput] = useState("");
-  const [deckCodes, setDeckCodes] = useState([]);
+  const [deckCodes, setDeckCodes] = useState([
+    "CQDQCAIEGQAQOBICAEEQKDQCAUCQWDYCAYCRYKICBEFACFADAMCQGBIGAIAQCBIDAEDAKIAA",
+    "CQDACAIEGQAQMARAAEEAICICAIBAGBQCAMCAGBICA4BBCFAFAEBQEFABAYDB2AIHAQEQCCAKAYBACAQMGEAQCBAECE",
+    "CQDACBQKE4AQQCQGAEEASAYCAQEQEBICAUFETQABAQEQUAIMBUKAGAIEBEDQCBYKBEAQSCQSAEAQOCQP",
+  ]);
+
   const maxDecks = 3;
 
   //Params: deckCode (State)
@@ -32,13 +34,11 @@ function DeckContainer() {
       .map((card) => {
         let currentSet = rtSets[card.set - 1];
         for (let i in currentSet) {
-          // console.log(currentSet[i].name)
           if (currentSet[i].cardCode === card.code) {
             card.name = currentSet[i].name;
-            card.region = card.code.substring(2,4);
-            console.log(card.region);
-            // card.img = currentSet[i].fullAbsolutePath;
+            card.region = card.code.substring(2, 4);
             card.cost = currentSet[i].cost;
+            card.art = currentSet[i].assets[0].fullAbsolutePath;
             break;
           }
         }
@@ -56,51 +56,61 @@ function DeckContainer() {
   }
 
   return (
-    <>
-      <label>
-        Add Deck Code:
-        <input
-          type="text"
-          className="deckCodeInput"
-          onChange={(e) => {
-            setTextInput(e.target.value);
-            console.log(e.target.value);
-          }}
-        ></input>
-        <button type="submit" onClick={addDeckCode}>
-          Submit
-        </button>
-      </label>
-          <div className="decksWrapper">
-      {deckCodes.map((deck, deckIndex) => {
-        deckCodes.filter(a => a === deckCodes[deckIndex])
-        return (
-          <>
-            <div className="deck">
-            <ul style={{listStyleType:"none", padding:0}}>
-              {prepareDeck(deck).map((card) => {
-                return <Card card={card} key={card.code} />;
-              })}
-            </ul>
-
-            <button key={deckIndex} className="removeDeckButton"
-              onClick={() => setDeckCodes(
-                deckCodes.filter(a => a !== deckCodes[deckIndex])
-                
-              )}
-            >
-              Remove DC
-            </button>
-            </div>
-          </>
-        );
-      })}
+    <div className="app">
+      <div className="addDeckSpan">
+        <label className="addDeckInnerItems">
+          Add Deck Code:
+          <input
+            type="text"
+            className="deckCodeInput"
+            onChange={(e) => {
+              setTextInput(e.target.value);
+              console.log(e.target.value);
+            }}
+          ></input>
+          <button type="submit" onClick={addDeckCode}>
+            Submit
+          </button>
+        </label>
       </div>
-    </>
+      <div className="decksWrapper">
+        {deckCodes.map((deck, deckIndex) => {
+          deckCodes.filter((a) => a === deckCodes[deckIndex]);
+          return (
+            <>
+              <div className="deck">
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                  {prepareDeck(deck).map((card) => {
+                    return <Card card={card} key={card.code} />;
+                  })}
+                </ul>
+
+                <button
+                  key={deckIndex}
+                  className="removeDeckButton"
+                  onClick={() =>
+                    setDeckCodes(
+                      deckCodes.filter((a) => a !== deckCodes[deckIndex])
+                    )
+                  }
+                >
+                  Remove DC
+                </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
 function Card({ card }) {
-  // console.log(card.name);
-  return (<div style={{borderRadius:"10px"}} className={card.region.toLowerCase()}><li className="card" key={card.name}>{card.name}</li></div>);
+  return (
+    <div style={{ borderRadius: "10px" }} className={card.region.toLowerCase()}>
+      <li className="card" key={card.name}>
+        {card.name}
+      </li>
+    </div>
+  );
 }
